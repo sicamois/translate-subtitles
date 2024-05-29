5f2d4ff8ccd867b702143f76cc2d76d13a59ccaa
Po <sicamois@gmail.com>
2024-05-29 22:56:14 +0200

    styling

--- src/components/TranslateSubtitles.tsx
+++ src/components/TranslateSubtitles.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { use, useActionState, useEffect, useState } from 'react';
+import { use, useActionState, useState } from 'react';
 import { useFormStatus } from 'react-dom';
 import { createFile, translate } from '@/app/actions';
 import { Label } from './ui/label';
@@ -15,8 +15,7 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/ui/table';
-import { Card } from './ui/card';
-import { Download } from 'lucide-react';
+import { Download, Languages } from 'lucide-react';
 import Link from 'next/link';
 import { Textarea } from './ui/textarea';
 
@@ -105,29 +104,39 @@ export default function TranslateSubtitles({
 
   return (
     <form className='w-full flex flex-col gap-2 p-4' action={formAction}>
-      <Card className='flex gap-2 p-2 items-center justify-center mb-4'>
-        <Label htmlFor='language' className='text-xl font-light'>
-          Langue
-        </Label>
-        <select
-          id='language'
-          name='language'
-          className='appearance-none px-4 py-2 bg-muted-foreground text-muted text-xl rounded-md'
-          defaultValue='FRA'
-          onChange={(e) =>
-            setLanguage(e.target.value as keyof typeof languages)
-          }
-        >
-          {Object.entries(languages).map(([key, value]) => (
-            <option key={key} value={key}>
-              {value}
-            </option>
-          ))}
-        </select>
-        <Button formAction={translateAction} className='text-lg'>
-          Traduire
-        </Button>
-      </Card>
+      <div className='relative flex gap-8 p-2 items-center justify-end mb-4'>
+        <div className='flex justify-evenly w-1/2'>
+          <div className='flex gap-2 items-center'>
+            <Label htmlFor='language' className='text-xl font-light'>
+              Langue
+            </Label>
+            <select
+              id='language'
+              name='language'
+              className='appearance-none h-10 px-4 bg-muted-foreground text-muted text-xl rounded-md'
+              defaultValue={language}
+              onChange={(e) =>
+                setLanguage(e.target.value as keyof typeof languages)
+              }
+            >
+              {Object.entries(languages).map(([key, value]) => (
+                <option key={key} value={key}>
+                  {value}
+                </option>
+              ))}
+            </select>
+          </div>
+          <Button
+            formAction={translateAction}
+            className='h-10 text-lg bg-gradient-to-r from-pink-400 to-purple-600 hover:from-pink-500 hover:to-purple-700 p-2 transition-colors duration-100 ease-in-out pointer-events-auto'
+          >
+            <div className='flex gap-1 items-center'>
+              <Languages className='h-5 w-5' />
+              <p className='font-medium drop-shadow'>Traduire</p>
+            </div>
+          </Button>
+        </div>
+      </div>
       <Table className='w-full rounded-t-md overflow-hidden'>
         <TableHeader className='text-lg font-medium bg-primary text-primary-foreground'>
           <TableRow>
