d16c0f3487dfa3ac9828455111b4b25674de4e0c
Po <sicamois@gmail.com>
2024-06-23 15:53:54 +0200

    refactor page transition with onUploadComplete

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -7,7 +7,7 @@ export default async function Home({
 }: {
   params: { lang: SuppportedLocale };
 }) {
-  const labelsDict = await getDictionary(lang); // en
+  const labelsDict = await getDictionary(lang);
   return (
     <main className="m-auto p-8 pt-0 sm:mt-48">
       <UploadFile labelsDict={labelsDict} />
--- src/components/UploadFile.tsx
+++ src/components/UploadFile.tsx
@@ -5,7 +5,7 @@ import { Label } from './ui/label';
 import Spinner from './Spinner';
 import { LabelsDictionary } from '@/app/dictionaries';
 import { useUploadToS3 } from '@sicamois/use-upload-to-s3';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { encrypt } from '@/lib/encryptionUtils';
 import { Switch } from './ui/switch';
@@ -13,6 +13,7 @@ import { Switch } from './ui/switch';
 const supportedLanguages = ['FRA', 'ESP', 'ARA', 'ITA', 'RUS'] as const;
 
 export function UploadFile({ labelsDict }: { labelsDict: LabelsDictionary }) {
+  const router = useRouter();
   const [languages, setLanguages] = useState<
     (typeof supportedLanguages)[number][]
   >(['FRA', 'ESP', 'ARA']);
@@ -23,18 +24,15 @@ export function UploadFile({ labelsDict }: { labelsDict: LabelsDictionary }) {
     {
       accept: '.fcpxml',
       sizeLimit: 50 * 1024 * 1024,
+      onUploadCompleteClient(s3key) {
+        encrypt(s3key).then((encryptedFilename) =>
+          router.push(
+            `/translate?file=${encryptedFilename}&langs=${languages.join(',')}`,
+          ),
+        );
+      },
     },
   );
-  const router = useRouter();
-  useEffect(() => {
-    if (!s3key) return;
-
-    encrypt(s3key).then((encryptedFilename) =>
-      router.push(
-        `/translate?file=${encryptedFilename}&langs=${languages.join(',')}`,
-      ),
-    );
-  }, [languages, router, s3key]);
 
   return (
     <form className="m-auto flex flex-col items-center gap-4">
