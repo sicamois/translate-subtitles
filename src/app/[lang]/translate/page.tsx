d9665a89c7518f9185232cbfaa47b1dc9b6d3ece
Po <sicamois@gmail.com>
2024-06-23 17:40:02 +0200

    insert subtitles into suspense

--- src/app/[lang]/translate/page.tsx
+++ src/app/[lang]/translate/page.tsx
@@ -4,7 +4,7 @@ import { exctractFCPXML } from '@/lib/fcpxmlUtils';
 import { getDictionary } from '@/app/dictionaries';
 import type { SuppportedLocale } from '@/app/dictionaries';
 import Title from '@/app/_components/Title';
-import { Suspense } from 'react';
+import { Suspense, use } from 'react';
 import Spinner from '@/components/ui/Spinner';
 
 export default async function Subtitles({
@@ -15,7 +15,7 @@ export default async function Subtitles({
   params: { lang: SuppportedLocale };
 }) {
   // It is inexpensive, so we can await it
-  // const labelsDict = await getDictionary(lang);
+  const labelsDictPromise = getDictionary(lang);
 
   const encryptedFilename = searchParams.file;
   if (!encryptedFilename || typeof encryptedFilename !== 'string') {
@@ -23,20 +23,18 @@ export default async function Subtitles({
   }
   const filename = decrypt(encryptedFilename);
 
-  const fcpxml = exctractFCPXML(filename);
+  const fcpxmlPromise = exctractFCPXML(filename);
 
   return (
     <main className="flex w-full flex-col items-center gap-6 pb-20">
-      <section className="flex flex-col items-center gap-2">
-        <div className="mt-8 h-16">
-          <Suspense fallback={<Spinner className="h-10 w-10" />}>
-            <Title fcpxml={fcpxml} />
-          </Suspense>
-        </div>
-        {/* <h2 className="text-center text-xl font-extralight drop-shadow-sm">
-          {labelsDict.translate.translateSubtitles}
-        </h2> */}
-      </section>
+      <div className="mt-8 h-20">
+        <Suspense fallback={<Spinner className="h-10 w-10" />}>
+          <Title
+            labelsDictPromise={labelsDictPromise}
+            fcpxmlPromise={fcpxmlPromise}
+          />
+        </Suspense>
+      </div>
     </main>
   );
 }
--- src/app/_components/Title.tsx
+++ src/app/_components/Title.tsx
@@ -1,13 +1,23 @@
 import { extractVideoTitle } from '@/lib/fcpxmlParser';
 import { FCPXML } from '@/lib/fcpxmlTypes';
 import { use } from 'react';
+import { LabelsDictionary } from '../dictionaries';
 
-export default function Title(props: { fcpxml: Promise<FCPXML> }) {
-  const fcpxml = use(props.fcpxml);
+export default function Title(props: {
+  fcpxmlPromise: Promise<FCPXML>;
+  labelsDictPromise: Promise<LabelsDictionary>;
+}) {
+  const labelsDict = use(props.labelsDictPromise);
+  const fcpxml = use(props.fcpxmlPromise);
   const videoTitle = extractVideoTitle(fcpxml);
   return (
-    <h1 className="px-4 text-center text-2xl font-light drop-shadow-sm sm:text-4xl">
-      {videoTitle}
-    </h1>
+    <section className="flex flex-col items-center gap-2">
+      <h1 className="px-4 text-center text-2xl font-light drop-shadow-sm sm:text-4xl">
+        {videoTitle}
+      </h1>
+      <h2 className="text-center text-xl font-extralight drop-shadow-sm">
+        {labelsDict.translate.translateSubtitles}
+      </h2>
+    </section>
   );
 }
