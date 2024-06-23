fad2302f1016720e88128b3942fba8dfe29548aa
Po <sicamois@gmail.com>
2024-06-23 16:51:27 +0200

    rm default loading component

--- src/app/[lang]/loading.tsx
+++
@@ -1,6 +0,0 @@
-import Spinner from '@/components/ui/Spinner';
-
-export default function Loading() {
-  // You can add any UI inside Loading, including a Skeleton.
-  return <Spinner className="mt-10 h-8 w-8" />;
-}
